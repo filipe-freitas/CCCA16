2d2b1867cfd49260a0e1218de551a24a2e0fd65d
Filipe Freitas <11577398+filipe-freitas@users.noreply.github.com>
2024-06-19 19:53:36 -0300

    feat: deve verificar se o email já existe e lançar um erro caso já exista

--- src/api.ts
+++ src/api.ts
@@ -11,38 +11,20 @@ app.post('/signup', async function (req, res) {
   try {
     const id = crypto.randomUUID()
 
-    const [acc] = await connection.query(
+    const [isEmailAlreadyUsed] = await connection.query(
       'select * from cccat16.account where email = $1',
       [req.body.email],
     )
-    if (!acc) {
-      if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-        if (req.body.email.match(/^(.+)@(.+)$/)) {
-          if (validate(req.body.cpf)) {
-            if (req.body.isDriver) {
-              if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-                await connection.query(
-                  'insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)',
-                  [
-                    id,
-                    req.body.name,
-                    req.body.email,
-                    req.body.cpf,
-                    req.body.carPlate,
-                    !!req.body.isPassenger,
-                    !!req.body.isDriver,
-                  ],
-                )
+    if (isEmailAlreadyUsed) {
+      res.status(400).json('Error: E-mail is already used') //result = -4;
+      return
+    }
 
-                const obj = {
-                  accountId: id,
-                }
-                result = obj
-              } else {
-                // invalid car plate
-                result = -5
-              }
-            } else {
+    if (req.body.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
+      if (req.body.email.match(/^(.+)@(.+)$/)) {
+        if (validate(req.body.cpf)) {
+          if (req.body.isDriver) {
+            if (req.body.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
               await connection.query(
                 'insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)',
                 [
@@ -60,22 +42,40 @@ app.post('/signup', async function (req, res) {
                 accountId: id,
               }
               result = obj
+            } else {
+              // invalid car plate
+              result = -5
             }
           } else {
-            // invalid cpf
-            result = -1
+            await connection.query(
+              'insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)',
+              [
+                id,
+                req.body.name,
+                req.body.email,
+                req.body.cpf,
+                req.body.carPlate,
+                !!req.body.isPassenger,
+                !!req.body.isDriver,
+              ],
+            )
+
+            const obj = {
+              accountId: id,
+            }
+            result = obj
           }
         } else {
-          // invalid email
-          result = -2
+          // invalid cpf
+          result = -1
         }
       } else {
-        // invalid name
-        result = -3
+        // invalid email
+        result = -2
       }
     } else {
-      // already exists
-      result = -4
+      // invalid name
+      result = -3
     }
     if (typeof result === 'number') {
       res.status(422).send(result + '')
--- test/api.test.ts
+++ test/api.test.ts
@@ -14,3 +14,35 @@ test('Deve criar uma conta para o passageiro', async function () {
   const output = await axios.post('http://localhost:3000/signup', input)
   console.log(output.status, output.data)
 })
+
+test('Não deve criar uma conta caso o e-mail já esteja em uso', async function () {
+  const sameEmail = `john.doe${Math.random()}@example.com`
+  const firstAccount = {
+    name: 'John Doe',
+    email: sameEmail,
+    cpf: '87748248800',
+    isPassenger: true,
+  }
+  const secondAccount = {
+    name: 'Jane Doe',
+    email: sameEmail,
+    cpf: '87748248801',
+    isPassenger: true,
+  }
+  const firstAccountSignup = await axios.post(
+    'http://localhost:3000/signup',
+    firstAccount,
+  )
+  console.log(`Status should be 200:${firstAccountSignup.status}`)
+  console.log(
+    `Property account_id should exist:${firstAccountSignup.data['accountId']}`,
+  )
+  const secondAccountSignup = await axios.post(
+    'http://localhost:3000/signup',
+    secondAccount,
+  )
+  console.log(`Status should be 400:${secondAccountSignup.status}`)
+  console.log(
+    `Error message should be 'Error: E-mail is already used':${secondAccountSignup.data}`,
+  )
+})
